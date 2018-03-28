609656aee4a70909dc4d350222627766b629f088
marialuisasab <marialuisasab@gmail.com>
2018-03-28 15:03:06 -0300

    CONSERTEI O CONFLITO

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-echo "Olá <strong>Mundo</strong>!!";
-=======
 echo "Hello <strong>World</strong>!!";
 echo "Hello <strong>World</strong> 2 !!";
 
->>>>>>> 2c431027d4468628716d7ed0715aa4462068dfad
 ?>
